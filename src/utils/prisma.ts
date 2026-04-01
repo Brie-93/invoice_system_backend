a82c31902386768d491534c83d0ac6c78099f8c8
_.belle._ <anubibrigit@gmail.com>
2026-04-02 01:03:16 +0300

    db online, stopped tracking dist folder

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,17 +1,6 @@
 // src/utils/prisma.ts
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-// 1. Configure the adapter with your XAMPP defaults
-const adapter = new PrismaMariaDb({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',       // Default XAMPP user
-  password: '',       // Default XAMPP password (empty)
-  database: 'design_db'
-});
-
-// 2. Pass the adapter to the Prisma Client
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
