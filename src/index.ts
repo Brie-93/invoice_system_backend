0a789c25007d388ee5adb051907931ae00e1c369
_.belle._ <anubibrigit@gmail.com>
2026-04-02 09:48:02 +0300

    added fronend cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-// src/index.ts
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
 import apiRoutes from './routes/apiRoutes';
-
 
 dotenv.config();
 
@@ -12,19 +10,24 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors()); // Allows frontend to talk to backend
-app.use(express.json()); // Allows backend to understand JSON
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://invoice-system-uyig.vercel.app',
+  ],
+  credentials: true,
+}));
+app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes); 
 app.use('/api/app', apiRoutes);
 
-
 app.get('/', (req, res) => {
   res.send('Interior Design API is running...');
 });
 
-app.get('/api/version', (req, res) => res.json({ ts: new Date().toISOString() }))
+app.get('/api/version', (req, res) => res.json({ ts: new Date().toISOString() }));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
